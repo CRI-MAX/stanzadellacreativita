aec6100313a9dfb07f2e31125c7c545861a0ff6c
Massimo Cristofori <cristoforimassimo@gmail.com>
2025-08-22 17:01:33 +0200

    Aggiornati render.yaml e script.js per migliorare deploy e logica frontend

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function sendMessage() {
   log.scrollTop = log.scrollHeight;
 
   // Invio al backend Python
-  fetch('http://localhost:5000/chat', {
+  fetch('http://stanzadellacreativita.onrender.com//chat', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ message: userText })
